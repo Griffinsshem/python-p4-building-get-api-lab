97f995525f238f9ac0bffb8428cbbf3bf7b5e13a
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-09-15 08:57:35 +0300

    feat: building a GET api

--- server/app.py
+++ server/app.py
@@ -20,19 +20,28 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries =  Bakery.query.all()
+    bakery_list = [bakery.to_dict() for bakery in bakeries]
+    return jsonify(bakery_list)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get_or_404(id)
+    return jsonify(bakery.to_dict())
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify([bg.to_dict() for bg in baked_goods])
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        return jsonify(most_expensive.to_dict()), 200
+    else:
+        return jsonify({"error": "No baked goods found"}), 404
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
